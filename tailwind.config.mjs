fb14175fd02ae89c71e99a6b244a60f5f179c6b6
Yaya <youmnael38@gmail.com>
2023-11-26 13:08:20 +0200

    Add Venus Page
    Add Venus Page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
+    './pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
